de7ffa07c83ae5758767c00f81aa59b355265df3
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-31 13:34:54 -0400

    Added dynamic port

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23' });
 });
 
+//the port is coming from the enviorment
+const port =process.env.PORT || 4000; //represents enviorment aplication is running
+//the PORT will be set by heroku when we deploy
+
 server.listen(4000, () => {
-  console.log('\n Server running on port 4000 \n');
+  console.log(`\n Server running on port ${port} \n`);
 });
--- node.md
+++ node.md
@@ -13,3 +13,9 @@ Dev Process
 -write code
 -commit and push
 -profit (it's deployed automatically to heroku)
+
+Steps to prepare API for deployment to Heroku
+
+-make the port dynamic 
+-setup a "start" script that uses "node" not nodemon to run on server
+The enviorment is the platform (the place/ operating system/machine) where the application is runnung.
